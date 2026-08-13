40d73ab11b243d2c2d9dd0a7aeb30274142fbef3
DevilX710 <tamzidzakir431@gmail.com>
2026-08-13 21:22:41 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Put your Supabase project values here.
 // Supabase dashboard → Project Settings → API
-window.SUPABASE_URL = "YOUR_SUPABASE_URL";
-window.SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+window.SUPABASE_URL = "https://telwykbrnnqqsbbhmbum.supabase.co/rest/v1/";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlbHd5a2Jybm5xcXNiYmhtYnVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2MjY0MjQsImV4cCI6MjEwMjIwMjQyNH0._fEIbHzyHtIGboT0Q6zAL2vMGXquQWj9I92yfmymF6Y";
